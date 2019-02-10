e4f5d5c22e5a010d5bc9b3488512f174a7e4d9c5
zack <77209302@qq.com>
2019-02-11 02:03:51 +0800

    update video page

--- lib/view/video_play/comment.dart
+++ lib/view/video_play/comment.dart
@@ -1,24 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
 
 class Comment extends StatefulWidget {
   @override
   _CommentState createState() => _CommentState();
 }
 
-class _CommentState extends State<Comment> {
+class _CommentState extends State<Comment> with TickerProviderStateMixin{
   final ScrollController _scrollController = new ScrollController(initialScrollOffset: 0.0);
 
   @override
   void initState() {
     super.initState();
-//    _scrollController..addListener(() {
-//      _scrollController.jumpTo(0.0);
-//    });
+    _scrollController..addListener(() {
+      print(_scrollController.position.pixels);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      onVerticalDragUpdate: (DragUpdateDetails details){
+        _scrollController.jumpTo(_scrollController.position.pixels + -details.delta.dy);
+      },
+      onVerticalDragEnd: (DragEndDetails details){
+        print(details.velocity.pixelsPerSecond);
+        AnimationController m = AnimationController(vsync: this);
+        VelocityTracker v = VelocityTracker();
+        m.addListener((){
+          _scrollController.jumpTo(_scrollController.position.pixels - details.primaryVelocity);
+          print('value:${m.value}-${details.primaryVelocity}-${details.velocity.pixelsPerSecond.direction}');
+        });
+        m.fling(velocity: details.velocity.pixelsPerSecond.dy);
+//        m.forward();
+      },
       child: ListView.builder(
         physics: new NeverScrollableScrollPhysics(),
         controller: _scrollController,
